dbba17b421753b30c58bb9950d8418d039943394
NeghiDB <neghianruwa@gmail.com>
2024-01-06 20:57:06 +0100

    Created user here

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ let users = [
 app.get('/users',(req, res) => {
     res.send(users);
 });
+app.post("/users", (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send({message: "User succesfully created", user, users})
+})
 
 app.listen(3000, () => {
     console.log("Neghi's app listening on port 3000!");
